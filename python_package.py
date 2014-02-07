48b0b296b01831abca0d4222ef456c82c6d151b8
Anderson Santos <santos@mir.de>
2014-02-07 18:35:39 +0100

    Added extension to the new file

--- python_package.py
+++ python_package.py
@@ -7,13 +7,16 @@ class CreatePackageCommand(sublime_plugin.WindowCommand):
         self.window.show_input_panel("Package Name:", "", functools.partial(self.on_done, dirs[0]), None, None)
 
     def on_done(self, dir, name):
-    	new_dir = os.path.join(dir, name)
-        os.makedirs(new_dir)
-        open(os.path.join(new_dir,"__init__.py"),'w').close()
+        new_dir = os.path.join(dir, name)
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+            open(os.path.join(new_dir,"__init__.py"),'w').close()
 
-        v = self.window.new_file()
+            v = self.window.new_file()
 
-        v.settings().set('default_dir', new_dir)
+            v.settings().set('default_dir', new_dir)
+            v.settings().set('default_extension', 'py')
+            v.set_syntax_file('Packages/Python/Python.tmLanguage')
 
     def is_visible(self, dirs):
         return len(dirs) == 1
